6b5e28a04e48499b5827d49bec469d9ec42350e9
Phuong-Ngoc1810 <tonguyenphuongngoc2008@gmail.com>
2024-01-14 17:36:25 +0700

    first commit

---
+++ trang2/main2.js
@@ -0,0 +1,8 @@
+function xinchao() {
+    let xemNoiDung = confirm("Bạn có muốn đăng nhập không?");
+    if (xemNoiDung) {
+        alert("Chào mừng bạn đến với RUBY");
+    } else {
+        alert("Bạn đã từ chối");
+    }
+}
