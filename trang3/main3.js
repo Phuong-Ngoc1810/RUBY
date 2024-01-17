0ef916b9b62e6635c11320e56667240fe081ee33
Phuong-Ngoc1810 <tonguyenphuongngoc2008@gmail.com>
2024-01-18 00:23:13 +0700

    third commit

--- trang3/main3.js
+++
@@ -1,8 +0,0 @@
-function xinchao() {
-    let xemNoiDung = confirm("Bạn có muốn đăng nhập không?");
-    if (xemNoiDung) {
-        alert("Chào mừng bạn đến với RUBY");
-    } else {
-        alert("Bạn đã từ chối");
-    }
-}
